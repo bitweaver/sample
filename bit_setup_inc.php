994dfea52f4ee7367ee45cb34e22834e5e50357b
Max Kremmel <xing@synapse.plus.com>
2005-12-10 10:53:28 +0000

    use PKG_DIR as key for registerAppMenu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,9 +1,9 @@
 <?php
 global $gBitSystem;
-$gBitSystem->registerPackage('sample', dirname(__FILE__).'/' );
+$gBitSystem->registerPackage( 'sample', dirname(__FILE__).'/' );
 
-if ($gBitSystem->isPackageActive('sample' ) ) {
-    $gBitSystem->registerAppMenu('sample', 'Sample', SAMPLE_PKG_URL.'index.php', 'bitpackage:sample/menu_sample.tpl', 'sample');
+if ($gBitSystem->isPackageActive( 'sample' ) ) {
+    $gBitSystem->registerAppMenu( SAMPLE_PKG_DIR, 'Sample', SAMPLE_PKG_URL.'index.php', 'bitpackage:sample/menu_sample.tpl', 'sample' );
 }
 
 ?>
