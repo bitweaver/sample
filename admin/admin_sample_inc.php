54237766e5ba5bf0ca09d6ea64795df09a42a2d9
Max Kremmel <xing@synapse.plus.com>
2006-02-08 21:51:15 +0000

    php: introduce new method: getPref( $package, $pref, $defaut ) (order is open for discussion tpl: $gBitPref.package.pref applied package name to all saves - should make database more organised and should allow more reliable uninstalls in the future

--- admin/admin_sample_inc.php
+++ admin/admin_sample_inc.php
@@ -1,15 +1,16 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_sample/admin/admin_sample_inc.php,v 1.6 2006/02/06 16:04:09 lsces Exp $
+// $Header: /cvsroot/bitweaver/_bit_sample/admin/admin_sample_inc.php,v 1.7 2006/02/08 21:51:15 squareing Exp $
 // Copyright (c) 2005 bitweaver Sample
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-if (isset($_REQUEST["sampleset"]) && isset($_REQUEST["homeSample"])) {
-    $gBitSystem->storePreference("home_sample", $_REQUEST["homeSample"]);
-    $gBitSmarty->assign('home_sample', $_REQUEST["homeSample"]);
-}
+// is this used?
+//if (isset($_REQUEST["sampleset"]) && isset($_REQUEST["homeSample"])) {
+//	$gBitSystem->storePreference("home_sample", $_REQUEST["homeSample"]);
+//	$gBitSmarty->assign('home_sample', $_REQUEST["homeSample"]);
+//}
 
-require_once(SAMPLE_PKG_PATH.'BitSample.php' );
+require_once( SAMPLE_PKG_PATH.'BitSample.php' );
 
 $formSampleLists = array(
 	"sample_list_sample_id" => array(
