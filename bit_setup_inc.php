55757f2eeebdb35efed6a71e2c5b9be481314d30
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:48 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,6 +1,11 @@
 <?php
 global $gBitSystem;
-$gBitSystem->registerPackage( 'sample', dirname(__FILE__).'/' );
+
+$registerHash = array(
+	'package_name' => 'sample',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'sample' ) ) {
 	$gBitSystem->registerAppMenu( SAMPLE_PKG_NAME, ucfirst( SAMPLE_PKG_DIR ), SAMPLE_PKG_URL.'index.php', 'bitpackage:sample/menu_sample.tpl', SAMPLE_PKG_NAME );
