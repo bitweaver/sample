bb6f2262548a5819d27e2a65529f85939f922988
wjames5 <will@tekimaki.com>
2008-10-21 02:38:21 +0000

    fix bad EditPerm refs

--- remove_sample.php
+++ remove_sample.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_sample/remove_sample.php,v 1.3 2008/10/20 21:40:11 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_sample/remove_sample.php,v 1.4 2008/10/21 02:38:21 wjames5 Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: remove_sample.php,v 1.3 2008/10/20 21:40:11 spiderr Exp $
+ * $Id: remove_sample.php,v 1.4 2008/10/21 02:38:21 wjames5 Exp $
  * @package sample
  * @subpackage functions
  */
@@ -26,7 +26,7 @@ if( !$gContent->isValid() ) {
 	$gBitSystem->fatalError( "No sample indicated" );
 }
 
-$gContent->verifyEditPermission();
+$gContent->verifyUpdatePermission();
 
 if( isset( $_REQUEST["confirm"] ) ) {
 	if( $gContent->expunge()  ) {
